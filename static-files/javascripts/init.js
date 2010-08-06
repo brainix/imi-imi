573cab2e34a71cb3c44286627b670c8e98d4559b
brainix <brainix@1bd47d8c-d334-11de-8b71-6d74aa18b05d>
2010-08-06 04:20:31 +0000

    fixed bug that displayed unintended tooltips

--- static-files/javascripts/init.js
+++ static-files/javascripts/init.js
@@ -42,7 +42,7 @@ $(function() {
 \*----------------------------------------------------------------------------*/
 
 function initTooltips() {
-    $("#profile .gravatar").tooltip();
+    $(".change_my_gravatar").tooltip();
     $("#following li img").tooltip();
     $("#followers li img").tooltip();
 }
